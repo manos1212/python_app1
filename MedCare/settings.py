48d017bc742967462937859e1b683c56bc00e7c4
manos1212 <59945502+manos1212@users.noreply.github.com>
2020-07-13 23:47:21 +0300

    Update settings.py

--- MedCare/settings.py
+++ MedCare/settings.py
@@ -23,9 +23,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = ')d4d!+!p5w$n+@ltjk^$@hk-dm^n6g_505fk)23vy)h=7+m(nh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.78.155.148']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_app'
+    'my_app',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -76,12 +77,18 @@ WSGI_APPLICATION = 'MedCare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'med_care',
-        'USER': 'root',
-        'PASSWORD': 'password',#''
-        'HOST': 'db', #localhost
-        'PORT': '3306'
+        'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.mysql'),
+  	'NAME': os.getenv('DATABASE_NAME', 'med_care'),
+        'USER': os.getenv('DATABASE_USER', 'root'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+        'HOST': os.getenv('DATABASE_SERVICE_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DATABASE_SERVICE_PORT', 3306)
+        #'NAME': 'app_db',
+        #'USER': 'app_db_user',
+        #'PASSWORD': 'app_db_pw',
+        #'HOST': 'mysql',
+        #'PORT': '3306'
+
     }
 }
 
@@ -110,17 +117,26 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Etc/GMT-3'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# 'http://34.78.155.148/'
+STATIC_URL = 'https://storage.googleapis.com/pyton_django_static/' 
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+CRISPY_TEMPLATE_PACK="bootstrap4"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login"
+
+MY_POD_NAME = os.getenv('MY_POD_NAME', 'local')
+MY_POD_NAMESPACE = os.getenv('MY_POD_NAMESPACE', 'local')
+MY_POD_IP = os.getenv('MY_POD_IP', 'localhost')
+
